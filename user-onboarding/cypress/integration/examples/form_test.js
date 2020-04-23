d8878004f7bebfe4ed6d9186b9a358adfe3c4f6d
Gabriel Romero <gabester78@gmail.com>
2020-04-23 08:41:33 -0500

    Opening structure of tests completed.

---
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -0,0 +1,6 @@
+describe("Testing our user onboarding form", function () {
+  beforeEach(function () {
+    cy.visit("http://localhost:3000/");
+  });
+  it("Add test to inputs and submit form", function () {});
+});
