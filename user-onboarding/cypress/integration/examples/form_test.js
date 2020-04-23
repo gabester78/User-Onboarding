5616c11f1d8b29a0cfd4b331afc760149e2ef792
Gabriel Romero <gabester78@gmail.com>
2020-04-23 08:56:49 -0500

    Name and email inputs tests passing!

--- user-onboarding/cypress/integration/examples/form_test.js
+++ user-onboarding/cypress/integration/examples/form_test.js
@@ -2,5 +2,10 @@ describe("Testing our user onboarding form", function () {
   beforeEach(function () {
     cy.visit("http://localhost:3000/");
   });
-  it("Add test to inputs and submit form", function () {});
+  it("Add test to inputs and submit form", function () {
+    cy.get("#name").type("Gabriel").should("have.value", "Gabriel");
+    cy.get("#email")
+      .type("gabester78@gmail.com")
+      .should("have.value", "gabester78@gmail.com");
+  });
 });
